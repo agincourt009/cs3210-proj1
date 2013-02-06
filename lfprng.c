7b08a9429c375010aae2e476cb6d1c999a045f37
agincourt009 <agincourt009@gmail.com>
2013-02-06 00:27:26 -0500

    Fixed the read_proc and write_proc functions. They are fully correct now.

--- lfprng.c
+++ lfprng.c
@@ -6,19 +6,65 @@
 #include <linux/sched.h>
 #include <asm/uaccess.h>
 
+struct process_id{
+    int pid;
+    struct list_head current_thread;
+    long long seed;
+    boolean updated;
+}
+
+struct thread_id
+{
+	int tid;     // This is tid the way you have it. To get the equivalent of tgid in their code, add a head pointer to the process_id struct
+	long long seed;
+	struct list_head next_thread;
+}
+
 static struct proc_dir_entry *proc_entry;
 
-int lfprng_read_proc(){
-}
+static int lfprng_read_proc(char *page, char **start, off_t off, int count, int *eof, void *data)
+{  
+	long long val = getRandNumber(current->tgid, current->pid, procID.seed);
 
-int lfprng_write_proc(){
-}
+	int length = sprintf(page, "%lld", val);
+
+  	return length;
+}//end lfprng_read_proc function
+
+static int lfprng_write_proc(struct file *file, const char *buf, usingned long count, void *data)
+{
+    	static const int SEED_SIZE = sizeof(long long);
+	long long seed;
+
+	if(count> SEED_SIZE)
+	{
+		count = SEED_SIZE;
+	}//end if statement
+        if(copy_from_user(&seed, buf, count))
+    	{
+    		return -EFAULT;
+    	}
+	setSeed(seed, current->tgid);
+	
+	return count;
+}//end lfprng_write_proc function
 
 void create_lfprng(){
 }
 
 void calc_lfprng(){
 }
+
+void create_New_Proc(int pid)//don't need to allocate memory for every thread. Just the first one, and then the rest will be handled by linked list
+{
+	procID.pid = pid;
+	procID.current_thread = newProc_first_thread;
+	for(rest of threads in newProc)
+	{
+		//add thread and it’s information
+	}
+	newProc.updated = true;
+}//end create_New_Proc function
 
 void seed(){
 //	get thread number => check what is omp_get_thread_num and omp_get_num_threads
